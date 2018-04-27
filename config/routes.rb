c3ff29cabd2f11b18056cc5c72186b16571b13ae
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-27 12:02:29 -0300

    controllers created for basic models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :assets do
+    resources :photos, only: [:new,:create, :edit, :update]
+  end
+  resources :photos, only: [:index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
