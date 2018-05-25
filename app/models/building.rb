9feb8c2ece783398a38b392e57a2b461e5a0ae3b
oscar ortiz <ortizg.oscar@gmail.com>
2018-05-25 13:51:05 -0300

    done fixing

--- app/models/building.rb
+++ app/models/building.rb
@@ -4,4 +4,5 @@ class Building < ApplicationRecord
   accepts_nested_attributes_for :photos
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  # after_validation :geocode
 end
